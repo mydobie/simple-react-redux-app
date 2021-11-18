f9c0da916870a7d194303c5873536470df1b8f9d
kimdobie <kimdobie@gmail.com>
2021-11-17 18:41:52 -0600

    Fix redirect tests

--- src/__tests__/pages/RedirectPage.test.tsx
+++ src/__tests__/pages/RedirectPage.test.tsx
@@ -1,56 +1,56 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable react/react-in-jsx-scope */
-// import { axe } from 'jest-axe';
-// import { render, screen, fireEvent, act } from '@testing-library/react';
-// import { Router } from 'react-router-dom';
-// import { createMemoryHistory } from 'history';
-// import RedirectPage from '../../pages/RedirectPage';
-// import ROUTES from '../../AppRouteNames';
+import { axe } from 'jest-axe';
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import RedirectPage from '../../pages/RedirectPage';
+import ROUTES from '../../AppRouteNames';
 
-// jest.setTimeout(6000);
+jest.setTimeout(6000);
 
-// function renderWithRouter(
-//   // @ts-ignore
-//   ui,
-//   {
-//     // @ts-ignore
-//     route = ROUTES.REDIRECT,
-//     history = createMemoryHistory({ initialEntries: [route] }),
-//   } = {}
-// ) {
-//   return {
-//     ...render(<Router history={history}>{ui}</Router>),
-//     history,
-//   };
-// }
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ...(jest.requireActual('react-router-dom') as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 describe('Redirect Page tests', () => {
-  test.todo('Redo react pages tests');
-  // test('Is accessible', async () => {
-  //   const { container, history } = renderWithRouter(<RedirectPage />);
-  //   await act(async () => {
-  //     const results = await axe(container);
-  //     expect(history.location.pathname).toEqual(ROUTES.REDIRECT);
-  //     expect(results).toHaveNoViolations();
-  //   });
-  // });
-  // test('User is forwarded to homepage after button click', () => {
-  //   const { history } = renderWithRouter(<RedirectPage />);
-  //   act(() => {
-  //     expect(history.location.pathname).toEqual(ROUTES.REDIRECT);
-  //     fireEvent.click(screen.getByTestId('goToHomeButton'));
-  //     expect(history.location.pathname).toEqual(ROUTES.HOME);
-  //   });
-  // });
-  // test('User is forwarded to homepage after 5 seconds', async () => {
-  //   const { history } = renderWithRouter(<RedirectPage />);
-  //   await act(async () => {
-  //     expect(history.location.pathname).toEqual(ROUTES.REDIRECT);
-  //     await new Promise((r) => setTimeout(r, 5500));
-  //     expect(history.location.pathname).toEqual(ROUTES.HOME);
-  //   });
-  // });
+  test('Is accessible', async () => {
+    const { container } = render(
+      <MemoryRouter>
+        <RedirectPage />
+      </MemoryRouter>
+    );
+    await act(async () => {
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    });
+  });
+  test('User is forwarded to homepage after button click', () => {
+    render(
+      <MemoryRouter>
+        <RedirectPage />
+      </MemoryRouter>
+    );
+    act(() => {
+      fireEvent.click(screen.getByTestId('goToHomeButton'));
+      expect(mockedUsedNavigate).toHaveBeenCalledWith(ROUTES.HOME);
+    });
+  });
+
+  test('User is forwarded to homepage after 5 seconds', async () => {
+    render(
+      <MemoryRouter>
+        <RedirectPage />
+      </MemoryRouter>
+    );
+    await act(async () => {
+      await new Promise((r) => setTimeout(r, 5500));
+      expect(mockedUsedNavigate).toHaveBeenCalledWith(ROUTES.HOME);
+    });
+  });
 });
 
-// https://www.rockyourcode.com/test-redirect-with-jest-react-router-and-react-testing-library/
-export {};
+// export {};
